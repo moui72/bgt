afb0753daf7fe26e4caf8a8c766c541ef9368529
Tyler Peckenpaugh <tyler@ty-pe.com>
2019-12-16 17:04:24 -0500

    query built, tableName fixt

--- bgt/buildAWSquery.py
+++ bgt/buildAWSquery.py
@@ -8,6 +8,8 @@ from schema import Game
 from utils import oxford_join, UniversalEncoder
 import datetime
 
+tableName = "GamesTest"
+
 with open('games.json', 'r') as jsongames:
     games = json.load(jsongames)
 
@@ -16,7 +18,7 @@ with open('designers.json', 'r') as jsondesigners:
 
 validGames = [Game(**game) for game in games.values()]
 
-query = {"Games": [game.asPutRequest() for game in validGames]}
+query = {tableName: [game.asPutRequest() for game in validGames]}
 
 print(query)
 
